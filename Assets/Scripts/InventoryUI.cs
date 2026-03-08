167f0ae22c4bc860bcba39ef7b392f5b519d622c
alarentsev <seney2@mail.ru>
2026-03-09 00:11:02 +0300

    Add inventory UI

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Inventory inventory;
+    public GameObject slotPrefab;
+    public Transform grid;
+    public ItemDefinition[] testItems;
+    private readonly List<SlotUI> slotUIs = new();
+    private ItemType? currentFilter;
+
+    private void Start()
+    {
+        BuildUI();
+        RefreshAll();
+    }
+
+    private void BuildUI()
+    {
+        for (var i = 0; i < inventory.slotCount; i++)
+        {
+            var go = Instantiate(slotPrefab, grid, false);
+            var s = go.GetComponent<SlotUI>();
+            slotUIs.Add(s);
+        }
+    }
+
+    public void RefreshAll()
+    {
+        for (var i = 0; i < slotUIs.Count; i++)
+        {
+            slotUIs[i].SetData(inventory.slots[i], i, this);
+            var visible = SlotPassesFilter(inventory.slots[i]);
+            slotUIs[i].SetVisible(visible);
+        }
+    }
+
+    private bool SlotPassesFilter(InventorySlot slot)
+    {
+        if (currentFilter == null) return true;
+        if (slot.IsEmpty) return true;
+        return slot.item.itemType == currentFilter.Value;
+    }
+
+    public void RequestRemoveFromSlot(int slotIndex)
+    {
+        inventory.RemoveFromSlot(slotIndex);
+        RefreshAll();
+    }
+
+    public void AddTestItemByIndex(int itemIndex)
+    {
+        if (itemIndex < 0 || itemIndex >= testItems.Length) return;
+        var ok = inventory.AddItem(testItems[itemIndex]);
+        if (!ok) Debug.Log("Не удалось добавить предмет");
+        RefreshAll();
+    }
+
+    public void ShowAll()
+    {
+        currentFilter = null;
+        RefreshAll();
+    }
+
+    public void ShowWeapons()
+    {
+        currentFilter = ItemType.Weapon;
+        RefreshAll();
+    }
+
+    public void ShowConsumables()
+    {
+        currentFilter = ItemType.Consumable;
+        RefreshAll();
+    }
+}
---
+++ Assets/Scripts/SlotUI.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotUI : MonoBehaviour
+{
+    public Image icon;
+    public TextMeshProUGUI countText;
+    public Button button;
+    private int index;
+    private InventoryUI parentUI;
+    private InventorySlot slot;
+
+    private void Start()
+    {
+        if (button != null) button.onClick.AddListener(OnClick);
+    }
+
+    public void SetData(InventorySlot s, int idx, InventoryUI ui)
+    {
+        slot = s;
+        index = idx;
+        parentUI = ui;
+        if (s.IsEmpty)
+        {
+            icon.enabled = false;
+            countText.text = "";
+        }
+        else
+        {
+            icon.enabled = true;
+            icon.sprite = s.item.icon;
+            countText.text = s.item.stackable ? s.count.ToString() : "";
+        }
+    }
+
+    private void OnClick()
+    {
+        if (slot == null || slot.IsEmpty) return;
+        parentUI.RequestRemoveFromSlot(index);
+    }
+
+    public void SetVisible(bool v)
+    {
+        gameObject.SetActive(v);
+    }
+}
